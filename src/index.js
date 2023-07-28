cf891007eecafd8c8d2bb865617a8890cdbb3fb2
max221088 <117590988+max221088@users.noreply.github.com>
2023-07-28 21:05:06 +0300

    Practice 3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const inputClick = () => console.log("Clicked")
 const mouseOver = () => console.log("Mouse Over")
 
 const helpText = "Help text"
+const container = "Text 1"
+const container2 = "Text 2"
 
 const elements = (<div className="name">
   <h1>{helpText}</h1>
@@ -12,7 +14,15 @@ const elements = (<div className="name">
     onClick={inputClick} onMouseEnter={mouseOver} />
   <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
 </div>)
-
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
 app.render(elements)
+
+const containerHtml = ReactDOMClient.createRoot(document.getElementById("container"))
+const containerHtml2 = ReactDOMClient.createRoot(document.getElementById("container-2"))
+
+containerHtml.render(<p>{container}</p>)
+containerHtml2.render(<div>
+  <p>{container}</p>
+  <p>{container2}</p>
+</div>)
